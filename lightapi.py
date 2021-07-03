7b619ef514965b52f8ac46b0db9a0249cd42992b
Theauxm <theaux.masquelier@gmail.com>
2021-07-03 10:28:37 -0600

    Created basic lightapi class

---
+++ lightapi.py
@@ -0,0 +1,32 @@
+import config
+from rpi_ws281x import *
+import time
+import random
+
+class lights:
+    def __init__(self):
+        self.strip = Adafruit_NeoPixel(
+                config.LED_COUNT,
+                config.LED_PIN,
+                config.LED_FREQ_HZ,
+                config.LED_DMA,
+                config.LED_INVERT,
+                config.LED_BRIGHTNESS
+                config.LED_CHANNEL)
+
+        self.pixels = [[0, 0 0] for i in range(strip.numPixels())]
+
+        strip.begin()
+
+
+    def update(clear=False):
+        for i in range (self.strip.numPixels()):
+            if clear:
+                self.strip.setPixelColor(i, 0)
+            else:
+                self.strip.setPixelColor(i, Color(self.pixels[i][0],
+                                                self.pixels[i][1],
+                                                self.pixels[i][2]))
+
+        self.strip.show()
+
